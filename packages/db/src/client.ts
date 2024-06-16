6e82fdbbed329718dbb04b18c48afb5f4b4e1fdc
vasyaqwe <elekton.fafik@gmail.com>
2024-06-16 12:23:10 +0300

    neon driver

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,19 +1,10 @@
-import { drizzle } from "drizzle-orm/postgres-js"
-import postgres from "postgres"
-
 import * as expenses from "./schema/expenses"
 import * as users from "./schema/users"
 import { env } from "./env"
+import { neon } from "@neondatabase/serverless"
+import { drizzle } from "drizzle-orm/neon-http"
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
-const globalForDb = globalThis as unknown as {
-   conn: postgres.Sql | undefined
-}
+const sql = neon(env.DATABASE_URL)
+const db = drizzle(sql, { schema: { ...expenses, ...users } })
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL)
-if (env.NODE_ENV !== "production") globalForDb.conn = conn
-
-export const db = drizzle(conn, { schema: { ...expenses, ...users } })
+export { db }
