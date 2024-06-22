5a5ab6a2ca1f0e89edf91cdf163b7bded3e4502f
vasyaqwe <elekton.fafik@gmail.com>
2024-06-22 12:29:50 +0300

    fix commands

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,15 +1,9 @@
-import dotenv from "dotenv"
 import type { Config } from "drizzle-kit"
-
-dotenv.config({
-   path: "../../.env",
-})
-
-if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set")
+import { env } from "./src/env"
 
 export default {
    schema: "./src/schema/**/*.ts",
    dialect: "postgresql",
-   dbCredentials: { url: process.env.DATABASE_URL },
+   dbCredentials: { url: env.DATABASE_URL },
    tablesFilter: ["acme_*"],
 } satisfies Config
